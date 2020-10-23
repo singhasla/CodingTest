86fc58e4908d70292ea3f041f1c391bc5704ee13
singhasla <s1n9hasla@gmail.com>
2020-10-23 22:42:07 +0900

    if문

---
+++ BeakJoon/src/STEP2/Step2_03.java
@@ -0,0 +1,22 @@
+package STEP2;
+
+import java.util.Scanner;
+
+public class Step2_03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		if(a%4==0){
+			if(a%100!=0 || a%400==0)
+				System.out.println("1");
+			else 
+				System.out.println("0");
+		} else {
+			System.out.println("0");
+		}
+	}
+
+}
---
+++ BeakJoon/src/STEP2/Step2_04.java
@@ -0,0 +1,26 @@
+package STEP2;
+
+import java.util.Scanner;
+
+public class Step2_04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(x>0){
+			if(y>=45)
+				System.out.println(x + " " + (y-45));
+			else
+				System.out.println((x-1) + " " + (y+15));
+		} else {
+			if(y>=45)
+				System.out.println("0" + " " + (y-45));
+			else
+				System.out.println("23" + " " + (y+15));
+		}
+	}
+
+}
