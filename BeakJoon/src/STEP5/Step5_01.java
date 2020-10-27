056eba04871499eb71df4c1f0343a29dc553cb15
singhasla <s1n9hasla@gmail.com>
2020-10-27 19:46:35 +0900

    1차원 배열 > Buffered 입출력 함수로 재작성

--- BeakJoon/src/STEP5/Step5_01.java
+++ BeakJoon/src/STEP5/Step5_01.java
@@ -1,30 +1,43 @@
 package STEP5;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Step5_01 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-				
-			int n = sc.nextInt();
-			int max=0, min=0;
-			int[] array = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int max=0, min=0;
+		
+		int t = Integer.parseInt(br.readLine());
+		int[] array = new int[t];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0;i<t;i++){
+			array[i] = Integer.parseInt(st.nextToken());
 			
-			for(int i=0;i<n;i++){
-				array[i] = sc.nextInt();
-				
-				if(i==0){
-					max=array[0];
-					min=array[0];
-				} 
-				else if(max<=array[i])
-					max=array[i];
-				else if(min>=array[i])
-					min=array[i];
-			}
+			if(i==0){
+				max=array[0];
+				min=array[0];
+			} 
+			else if(max<=array[i])
+				max=array[i];
+			else if(min>=array[i])
+				min=array[i];
+		}
 			
-			System.out.println(min + " " + max);
+		bw.write(min + " " + max);
+		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 
 }
