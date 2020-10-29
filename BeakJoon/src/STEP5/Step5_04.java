321dfd9d6a8a91b1c2794dd371fbf255b084f84b
singhasla <s1n9hasla@gmail.com>
2020-10-29 17:52:43 +0900

    1차원 배열

---
+++ BeakJoon/src/STEP5/Step5_04.java
@@ -0,0 +1,36 @@
+package STEP5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Step5_04 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int[] num = new int[10];
+		int[] count = new int[42];
+		int cnt = 0;
+
+		for (int i = 0; i < 10; i++) {
+			int a = Integer.parseInt(br.readLine());
+			count[a % 42] += 1;
+		}
+
+		for (int i = 0; i < 42; i++) {
+			if (count[i] >= 1)
+				cnt += 1;
+		}
+
+		bw.write(String.valueOf(cnt));
+
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
---
+++ BeakJoon/src/STEP5/Step5_05.java
@@ -0,0 +1,33 @@
+package STEP5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Step5_05 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		char[] array = new char[80];
+			
+		for(int i=0;i<array.length;i++) {
+			
+			String line = br.readLine();
+		
+		}
+		
+		//bw.write();
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+}
