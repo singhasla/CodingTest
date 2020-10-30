21a28997a085ce3fc27126b63ca12185501fa79f
singhasla <s1n9hasla@gmail.com>
2020-10-30 10:36:58 +0900

    1차원 배열

--- BeakJoon/src/STEP5/Step5_06.java
+++ BeakJoon/src/STEP5/Step5_06.java
@@ -5,34 +5,38 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
-public class Step5_05 {
+public class Step5_06 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		double max=0, sum=0;
 		
+		int total =0;
 		int n = Integer.parseInt(br.readLine());
-		double[] score = new double[n];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		for(int i=0;i<n;i++){
-			score[i] = Integer.parseInt(st.nextToken());
+		for(int i=0;i<n;i++) {
+			String line = br.readLine();
+			int[] ox = new int[line.length()];
+
+			for(int j=1;j<ox.length;j++){
+				if(line.charAt(0)=='O')
+					ox[0]=1;
+				else if(line.charAt(0)=='X')
+					ox[0]=0;
+				
+				if(line.charAt(j)=='O')
+					ox[j]=ox[j-1]+1;
+				else if(line.charAt(j)=='X')
+					ox[j]=0;
+			}	
 			
-			if(score[i]>max)
-				max = score[i];
-		}
-		
-		for(int i=0;i<n;i++){
-			score[i] = score[i]/max*100;
-			sum+=score[i];
-		}
+			for(int k=0;k<ox.length;k++) {
+				total = total + ox[k];
+			}
 			
-		bw.write(String.valueOf(sum/n));
+			bw.write(String.valueOf(total)+"\n");
+		}
 		
 		br.close();
 		bw.flush();
